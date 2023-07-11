726e4f44aa5e365278c5e8dcdf66209d3e3fce56
Steven Lawson <steven.lawson@healthsqyre.com>
2023-07-11 16:56:27 -0600

    Header and Footer components added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <template>
-	<div>Hello</div>
+	<app-header></app-header>
+	<div>Hello {{ name }}</div>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				name: 'Arya'
+			}
+		}
+	}
 </script>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-size: 10px;
+	}
+
+	div {
+		color:orangered;
+	}
+</style>
---
+++ src/components/header-footer/Header.vue
@@ -0,0 +1,13 @@
+<template>
+	<header>
+		header
+	</header>
+</template>
+
+<style>
+	header {
+		width: 100%;
+		background-color: aqua;
+		font-size: 4rem;
+	}
+</style>
