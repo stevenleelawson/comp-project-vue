91d9245dd6525e83bcb0973a58d691f196059b39
Steven Lee Lawson <stevenleelawson@gmail.com>
2023-07-11 16:57:13 -0600

    Header and Footer components added (#1)
    Co-authored-by: Steven Lawson <steven.lawson@healthsqyre.com>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue';
 import App from './App';
 
-createApp(App).mount('#app');
+// globally register components
+import Header from '../src/components/header-footer/Header';
+
+const app = createApp(App);
+
+app.component('app-header', Header);
+
+app.mount('#app');
