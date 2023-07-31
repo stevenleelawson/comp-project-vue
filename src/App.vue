3b4de740593016fbc993789032532ed476406a03
Steven Lee Lawson <stevenleelawson@gmail.com>
2023-07-31 16:19:56 -0600

    Components and styling (#2)
    * Header and Footer components added
    
    * adding components and wicked awesome styling
    
    * added UserProfile component and styling it more betterz
    
    * event bubbling via emit
    
    ---------
    
    Co-authored-by: Steven Lawson <steven.lawson@healthsqyre.com>

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,51 @@
 <template>
 	<app-header></app-header>
-	<div>Hello {{ name }}</div>
+	<div class="container">Hello {{ name }}
+		<!-- kebab here::::: -->
+		<div class="container">
+			<compu-user-profile 
+				:name="name" 
+				:aka="aka"
+				:userAge="userAge"
+				:userParents="userParents"
+				@update-name="aka = $event"
+				@say-hello="alertHello"
+				:updateAge="updateAge"
+			></compu-user-profile>
+			<button @click="updateName">update nombre</button>
+		</div>
+	</div>
+	<app-footer></app-footer>
 </template>
 
 <script>
+	import Footer from './components/header-footer/Footer.vue';
+	import compuUserProfile from './User/Profile.vue';
+
 	export default {
+		components: {
+			'app-footer': Footer,
+			// REMEMBER: Vue will automatically make it kebab-case under the hood::::::::::
+			compuUserProfile,
+		},
 		data() {
 			return {
-				name: 'Arya'
+				name: 'Arya',
+				aka: 'Cookie Teapot',
+				userAge: 3,
+				userParents: { daddy: 'Steven', mommy: 'None yet'}
+			}
+		},
+		methods: {
+			updateName() {
+				this.name = 'chipchip'
+			},
+			alertHello(e) {
+				console.log('eeee', e)
+				alert(e.target.innerText)
+			},
+			updateAge(value) {
+				this.userAge = value;
 			}
 		}
 	}
@@ -19,8 +57,16 @@
 		padding: 0;
 		box-sizing: border-box;
 		font-size: 10px;
+		font-family: 'Roboto', sans-serif;
 	}
 
+	.container {
+		font-size: 2rem;
+		/* min-height: 84vh; */
+		padding: 20px;
+		background-image: linear-gradient(to right bottom, #eb2f64, #BA265D);
+		color: #333;
+	}
 	div {
 		color:orangered;
 	}
--- src/components/header-footer/Footer.vue
+++ src/components/header-footer/Footer.vue
@@ -3,3 +3,10 @@
 		Footer
 	</footer>
 </template>
+
+<style>
+	footer {
+		background-image: linear-gradient(to right bottom, olivedrab, chartreuse);
+		height: 16vh;
+	}
+</style>
