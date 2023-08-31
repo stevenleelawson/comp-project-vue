10dec252815cc43ed7dc61001dae3c89d790d596
Steven Lawson <steven.lawson@healthsqyre.com>
2023-08-31 16:50:46 -0600

    declaring custom directive locally

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
 	<app-header></app-header>
 	<div class="container">
 		<div v-awesome="'Lil Baby'"></div>
-		<h1>arya bnada</h1>
+		<div v-user-lastname="'RUh roh'"></div>
+		<h1>arya</h1>
 		<compLife></compLife>
 	</div>
 	<app-footer></app-footer>
@@ -15,6 +16,20 @@ import compLife from './components/Life';
 import appFooter from './components/header-footer/Footer.vue'
 
 export default {
+	data() {
+		return {
+			lastName: 'Lawson'
+		}
+	},
+	// HOW TO DECLARE A CUSTOM DIRECTIVE LOCALLY IN THE COMPONENT
+	directives: {
+		'user-lastname': {
+			// NOTE YOU CAN USE LIFECYCLE HOOKS in Custom Directives
+			beforeMount(el, binding) {
+				el.innerHTML = binding.value
+			}
+		}
+	},
 	components: {
 		compLife,
 		appFooter
